8871d06597f0356e20f6c6b64b52219e1254aab2
zonetecde <zonetecde@gmail.com>
2025-03-03 14:24:11 +0100

    /

--- src/routes/api/get-association/+server.ts
+++ src/routes/api/get-association/+server.ts
@@ -48,14 +48,19 @@ export async function GET({ url }: { url: URL }) {
 	return await askGpt(firstWord, secondWord);
 }
 async function askGpt(firstWord: string, secondWord: string) {
+	let text: string | undefined;
 	try {
 		let response = await fetch(
 			`http://localhost:6969/gpt?prompt=🎲 Here's a challenge for you in a word combination game! You'll receive two words, and you'll need to give me the word that best fits the combination of these two. Respond with a single word and use an emoji that closely resembles the word you're writing.\n\nExample: if I give you "Chocolate" and "Store", your response should be: "Chocolaterie" with the emoji 🍫. You can provide an adjective, a noun, a proper noun, a city, a movie, a game, etc., but make sure the word exists in the dictionary.\nWhen both words are identical, you can propose an 'amplified' version of the thing. For example, for "rich" + "rich", you could answer: "The richest".\n\nWrite your response in JSON format, with two attributes: emoji (string) and name (string, representing the word in FRENCH).\n\nThe two words to combine are '${firstWord}' and '${secondWord}'. Don't forget to choose a relevant emoji for your response! If your response is 'Tornade' for example, your emoji should be 🌪️\n\n\n\nTwo same words should not give the same word in plural. Moreover, two words should not be fused together, like for exemple : earth and air should not give earth-air, but for exemple, wind. You should ALWAYS give a different word than the words I gave you. Moreover, the anwser can be an action. For exemple, "Sapeur-Pompier" with "incendie" should be "Sauvegate". Incendie et crash devrait donner 'Catastrophe'. If the words are so different that you cannot find of any combination, give as a result one of the word I gave you.`
 		);
 
-		let text = await response.json();
-
+		text = await response.json();
+		//```json\n{\n  \"emoji\": \"👨‍👩‍👧‍👦\",\n  \"name\": \"Clan\"\n}\n```\n
 		// Enlève si nécessaire l'indicateur json
+
+		// remove all the new lines
+		text = text.replace(/\n/g, '');
+
 		if (text.startsWith('```json')) text = text.slice(7, -3);
 		else if (text.startsWith('```')) text = text.slice(3, -3);
 
@@ -86,6 +91,7 @@ async function askGpt(firstWord: string, secondWord: string) {
 
 		return new Response(JSON.stringify(new Item(item[0].emoji, item[0].name, item[1])));
 	} catch (e: any) {
-		return new Response('none' + e.toString());
+		if (!text) return new Response('none' + e.toString() + 'no json');
+		return new Response('none' + e.toString() + text);
 	}
 }
